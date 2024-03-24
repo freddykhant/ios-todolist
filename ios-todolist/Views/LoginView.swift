98fd4137b70dc81830d972d54cc24898377a762c
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-25 02:50:43 +0800

    started creating the main view, and log in view

--- ios-todolist/Views/LoginView.swift
+++ ios-todolist/Views/LoginView.swift
@@ -5,4 +5,54 @@
 //  Created by Freddy Khant on 25/3/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    
+    var body: some View {
+        VStack {
+            // Header
+            HeaderView()
+            
+            // Login Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    // Attempt login
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+            }
+            
+            // Create account
+            VStack {
+                Text("New here?")
+                Button("Create an account") {
+                    // Show registration
+                }
+            }
+            //.padding(.bottom, 50)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}

1f57f7dc6a94ce7e23e18faf1b8896fae8e1dd70
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-25 01:23:21 +0800

    Initial Commit

---
+++ ios-todolist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-todolist
+//
+//  Created by Freddy Khant on 25/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-todolist/ios_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_todolistApp.swift
+//  ios-todolist
+//
+//  Created by Freddy Khant on 25/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

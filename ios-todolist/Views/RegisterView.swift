9d5a5f8def26af35af5b5a978abfd98ca856cc13
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-26 18:51:14 +0800

    register screen now done

--- ios-todolist/Views/RegisterView.swift
+++ ios-todolist/Views/RegisterView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView(title: "Register", subtitle: "Start organizing your list", angle: -15, background: .cyan)
+            
+            // Login Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TextField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                Button {
+                    // Attempt login
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Sign up")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+            }
+            .padding()
+            .offset(y:-50)
+            
+            // Create account
+            VStack {
+                Text("Already have one?")
+                
+                NavigationLink("Sign in",
+                               destination: LoginView())
+            }
+            //.padding(.bottom, 50)
             
             Spacer()
         }

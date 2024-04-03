fdde83fa18096dabf06f90c28f854a9c2ca896e2
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-04-04 02:08:54 +0800

    profile and home views

--- ios-todolist/Views/NewItemView.swift
+++ ios-todolist/Views/NewItemView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
